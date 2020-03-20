90dd4d3ae983b6fcdcb064ac81f49297c67e1f13
BlackSamorez <andrei@panferov-sci.org>
2020-03-20 20:56:18 +0300

    Обработка ответа. Сохранение ответа в файл.

--- Code/classes.py
+++ Code/classes.py
@@ -16,15 +16,60 @@ class QBM:
 		self.coef = {x: {y: 0 for y in self.ind} for x in self.ind}
 		self.sampler = EmbeddingComposite(DWaveSampler())
 		self.response = 0
+		self.data = []
+		self.datan = 0
 
 	def start(self, n = 1):
+		self.datan = n
 		Q = {(i, j): self.coef[i][j] for i in self.ind for j in self.ind}
+
 		self.response = self.sampler.sample_qubo(Q, num_reads=n)
 
+	def fetch_data(self):
+		self.data = []
+
+		for datum in self.response.data(fields = ['sample', 'energy', 'num_occurrences'], sorted_by = 'energy'):
+			sample = datum.sample
+			energy = datum.energy
+			percentage = round(datum.num_occurrences / self.datan, 4)
+			self.data += [{'sample': sample,'energy': energy,'percentage': percentage}]
+
+	def save_data(self, filename = 'last_data'):
+		filename = '../Data/' + filename
+		data_np = np.empty((len(self.data), 3), dtype = 'object')
+
+		for i in range(len(self.data)):
+			index = list(self.data[i]['sample'])
+			for key in index:
+				data_np[i][0] = key + '=' + str(self.data[i]['sample'][key]) + ','
+
+			data_np[i][0] = self.data[i]['sample']
+			data_np[i][1] = self.data[i]['energy']
+			data_np[i][2] = self.data[i]['percentage']
+
+		np.savetxt(filename, data_np, fmt='%s')
+
+def test():
+	a = QBM(2, 0)
+	a.coef['h0']['h0'] = -1
+	a.coef['h1']['h1'] = -1
+	a.coef['h0']['h1'] = 2
+	return a
 
 
-a = QBM(10,10)
-print('0')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
