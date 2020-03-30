e7d388283f26806fad0641879a98f2efcf490b99
BlackSamorez <andrei@panferov-sci.ru>
2020-03-30 18:54:58 +0300

    Rewritten data refinement inclining towards numpy

--- Code/classes.py
+++ Code/classes.py
@@ -27,7 +27,9 @@ class QBM:
 		self.response = 0	#response and data from it
 		self.datalen = 0
 		self.data = []
+		self.data_prob = []
 		self.datan = 0
+		self.prob_vec = []
 
 		dt = np.dtype(np.bool)	#image from set and its relation to vis layer coefs (curve)
 		self.image = np.empty((20, 20), dtype = dt)
@@ -61,6 +63,7 @@ class QBM:
 			self.datan = n
 			self.Q = {(i, j): self.coef[i][j] for i in self.ind for j in self.ind}
 			self.response = self.sampler.sample_qubo(self.Q, num_reads=n)
+			#self.response = self.response.data(fields = ['sample', 'num_occurrences'], sorted_by = 'sample')
 		else:
 			print("it's a test, can't run on dwave")
 
@@ -88,78 +91,39 @@ class QBM:
 
 
 	def fetch_data(self):	#reading response
-		self.data = []
-
+		self.datalen = 0
 		for datum in self.response.data(fields = ['sample', 'energy', 'num_occurrences'], sorted_by = 'energy'):
-			sample = datum.sample
-			energy = datum.energy
-			percentage = round(datum.num_occurrences / self.datan * 100, 1)
-			for i in sample:
-				sample[i] = bool(sample[i])
-			self.data += [{'sample': sample,'energy': energy,'percentage': percentage}]
-			self.datalen = len(self.data)
+			self.datalen += 1
+		self.data = np.zeros((self.datalen, self.hidlen + self.vislen), dtype = "int8")
+		self.data_prob = np.zeros(self.datalen, dtype = np.float_)
+		
+		i = 0
+		for datum in self.response.data(fields = ['sample', 'energy', 'num_occurrences'], sorted_by = 'energy'):
+			self.data_prob[i] = datum.num_occurrences / self.datan
+			for j in range(self.hidlen + self.vislen):
+				self.data[i][j] = datum.sample[self.ind[j]]
+			i += 1
 
-	def save_data_txt(self, filename = 'last_data'):	#saving response as txt
-		filename = '../Data/' + filename
-		data_np = np.empty((self.datalen, 3), dtype = 'object')
+	def save_data(self, filename = "test"):
+		filename1 = "../Data/" + filename + ".samples"
+		filename2 = "../Data/" + filename + ".probs"
+		filename3 = "../Data/" + filename + ".lens"
 
-		for i in range(self.datalen):
-			index = list(self.data[i]['sample'])
-			for key in index:
-				data_np[i][0] = key + '=' + str(self.data[i]['sample'][key]) + ','
+		lens = np.zeros((2), dtype = "int16")
+		lens[0] = self.hidlen
+		lens[1] = self.vislen
 
-			data_np[i][0] = self.data[i]['sample']
-			data_np[i][1] = self.data[i]['energy']
-			data_np[i][2] = self.data[i]['percentage']
-
-		np.savetxt(filename + '.txt', data_np, fmt='%s')
-
-	def save_data_idx(self, filename = 'last_data'):	#saving response as idx
-		filename = '../Data/' + 'sas'
-		dt = np.dtype(bool)
-		hid_np = np.empty((self.datalen, self.hidlen), dtype = dt)
-		vis_np = np.empty((self.datalen, self.vislen), dtype = dt)
-		ind_np = np.empty((self.datalen, 4), dtype = np.float)
-		for i in range(self.datalen):
-			for ind in range(self.hidlen):
-				if self.data[i]['sample'][self.hind[ind]] == 1:
-					k = 1
-				else:
-					k = 0
-				hid_np[i][ind] = k
-			for ind in range(self.vislen):
-				if self.data[i]['sample'][self.vind[ind]] == 1:
-					k = 1
-				else:
-					k = 0
-				vis_np[i][ind] = k
-			ind_np[i][0] = self.hidlen
-			ind_np[i][1] = self.vislen
-			ind_np[i][2] = self.data[i]['energy']
-			ind_np[i][3] = self.data[i]['percentage']
-			if ind_np != []:
-				idx2numpy.convert_to_file(filename + '_ind.idx', ind_np)
-			if hid_np != []:
-				idx2numpy.convert_to_file(filename + '_hid.idx', hid_np)
-			if vis_np != []:	
-				idx2numpy.convert_to_file(filename + '_vis.idx', vis_np)
-
-	def save_data_json(self, filename = 'last_data'):	#saving response as json
-		filename = '../Data/' + filename
-		with open(filename + '_json', 'w') as fout:
-			json.dump(self.data, fout) 
-
+		idx2numpy.convert_to_file(filename1, self.data)
+		idx2numpy.convert_to_file(filename2, self.data_prob)
+		idx2numpy.convert_to_file(filename3, lens)
 
 
 def test():
-	a = QBM(2, 0)
+	a = QBM(2, 0, True)
 	a.coef['h0']['h0'] = -1
 	a.coef['h1']['h1'] = -1
 	a.coef['h0']['h1'] = 2
 	return a
-
-
-
 
 
 
